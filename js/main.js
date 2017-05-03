5a1c0577fec414c3e542b7b6b085bbb73ba20135
Sarah Westbrook <sjwestbrook@users.noreply.github.com>
2017-05-03 14:11:58 -0600

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,3 +1,6 @@
+// when the #main_nav is clicked, the class "active" will be added to #main_nav
+// the a tags in the main nav are styled under media queries to display appropriately for mobile devices
+
 $(document).ready(function() {
   
   $('.menu').click(function(e) {
