aa6af91d69cbe57f1f2e39a274a0dd0ebd164c39
Sarah Westbrook <sjwestbrook@users.noreply.github.com>
2017-05-03 14:45:33 -0600

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,11 +1,16 @@
 // when the #main_nav is clicked, the class "active" will be added to #main_nav
 // the a tags in the main nav are styled under media queries to display appropriately for mobile devices
+// click again and the active class will be removed from #main_nav
 
 $(document).ready(function() {
   
   $('.menu').click(function(e) {
     e.stopPropagation(); 
     $('#main_nav').toggleClass('active');
+  });
+  
+  $(document).click(function() {
+    $('#main_nav').removeClass('active');
   });
   
 });
